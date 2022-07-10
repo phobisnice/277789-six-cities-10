08fb5ad818ab32a4e5186166e51d02474e48c8ad
Alexey Gukov <alexeygukov@yandex.ru>
2022-07-10 16:42:09 +0300

    Добавил компоненты страниц и карточки локации

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  CARDS_TO_SHOW: 5,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardsCount = {Setting.CARDS_TO_SHOW}
+    />
   </React.StrictMode>,
 );
